69f0ef2198167762d7b8c72fe611451eaa6cb87c
ProjetosMautex <murilogxmautex@gmail.com>
2026-02-26 11:19:18 -0300

    Adicionando adaptador da Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind'; // Certifique-se de que essa linha existe
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   integrations: [
     react(), 
     tailwind() // E que ele está aqui dentro
   ],
+
+  adapter: vercel(),
 });
