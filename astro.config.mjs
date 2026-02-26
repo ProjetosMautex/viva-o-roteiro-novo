c21cfff7b1f6820bc5da1f4af9fb6aacf219be1a
ProjetosMautex <murilogxmautex@gmail.com>
2026-02-26 11:24:37 -0300

    Configurando output server e ajuste tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind'; // Certifique-se de que essa linha existe
-
+import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  // Define que o site será renderizado no servidor (Vercel)
+  output: 'server',
+  
   integrations: [
     react(), 
-    tailwind() // E que ele está aqui dentro
+    tailwind({
+      applyBaseStyles: false, // Evita duplicidade de CSS no build
+    })
   ],
 
   adapter: vercel(),
